bdcaf7d50edfac5f4662b2513403e3937a4fe7a4
Andres Porras <aporrasmdz@gmail.com>
2024-10-27 17:31:16 -0500

    feat: dynamic section loading

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Función para cargar contenido HTML de archivos externos
     async function loadContent(route) {
         try {
-            const response = await fetch(`${route}.html`);
+            const response = await fetch(`pages/${route}.html`);
             if (response.ok) {
                 const html = await response.text();
                 content.innerHTML = html;
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
     navLinks.forEach(link => {
         link.addEventListener("click", event => {
             event.preventDefault();
-            const route = event.target.getAttribute("href").substring(1); // Obtener la ruta sin '#'
+            const route = event.target.getAttribute("href").substring(1); // Obtener la ruta sin '/'
             loadContent(route); // Cargar contenido basado en la ruta
         });
     });
