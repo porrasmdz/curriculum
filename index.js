3e6731a19d11d990039832ef3744eeffc9f92aee
Andres Porras <aporrasmdz@gmail.com>
2024-10-30 03:07:27 -0500

    fix: refactor cascade page

--- index.js
+++ index.js
@@ -7,34 +7,34 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     
-    const content = document.querySelector(".page-content");
-    const navLinks = document.querySelectorAll(".nav-link");
+    // const content = document.querySelector(".page-content");
+    // const navLinks = document.querySelectorAll(".nav-link");
 
-    // Función para cargar contenido HTML de archivos externos
-    async function loadContent(route) {
-        try {
-            const response = await fetch(`pages/${route}.html`);
-            if (response.ok) {
-                const html = await response.text();
-                content.innerHTML = html;
-            } else {
-                content.innerHTML = `<p>Lo siento, no se pudo cargar la página de ${route}.</p>`;
-            }
-        } catch (error) {
-            console.error("Error al cargar contenido:", error);
-            content.innerHTML = `<p>Ocurrió un error al intentar cargar el contenido de ${route}.</p>`;
-        }
-    }
+    // // Función para cargar contenido HTML de archivos externos
+    // async function loadContent(route) {
+    //     try {
+    //         const response = await fetch(`pages/${route}.html`);
+    //         if (response.ok) {
+    //             const html = await response.text();
+    //             content.innerHTML = html;
+    //         } else {
+    //             content.innerHTML = `<p>Lo siento, no se pudo cargar la página de ${route}.</p>`;
+    //         }
+    //     } catch (error) {
+    //         console.error("Error al cargar contenido:", error);
+    //         content.innerHTML = `<p>Ocurrió un error al intentar cargar el contenido de ${route}.</p>`;
+    //     }
+    // }
 
-    // Añadir evento de clic a cada link de navegación
-    navLinks.forEach(link => {
-        link.addEventListener("click", event => {
-            event.preventDefault();
-            const route = event.target.getAttribute("href").substring(1); // Obtener la ruta sin '/'
-            loadContent(route); // Cargar contenido basado en la ruta
-        });
-    });
+    // // Añadir evento de clic a cada link de navegación
+    // navLinks.forEach(link => {
+    //     link.addEventListener("click", event => {
+    //         event.preventDefault();
+    //         const route = event.target.getAttribute("href").substring(1); // Obtener la ruta sin '/'
+    //         loadContent(route); // Cargar contenido basado en la ruta
+    //     });
+    // });
 
-    // Cargar contenido inicial para 'home'
-    loadContent("home");
+    // // Cargar contenido inicial para 'home'
+    // loadContent("home");
 });
